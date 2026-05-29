93a286681bae0aa850563206c193ed875340287d
kisevu <amedakevin@gmail.com>
2026-05-30 01:08:48 +0300

    test

---
+++ src/test/java/com/ameda/kev/tanatech/TanaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameda.kev.tanatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanaTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
